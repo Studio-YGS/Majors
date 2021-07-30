fe199ebc0e049f94f086c9905702cc4699fb6f96
Jack <1018019@student.sae.edu.au>
2021-07-30 15:08:50 +1000

    added a way to outline objects and make the outline disappear at a certain distance

---
+++ Mr Crossy/Assets/QuickOutline/Scripts/OutlineSwitch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineSwitch : MonoBehaviour
+{
+    Outline outline;
+    Camera cam;
+    public float distanceAway;
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        cam = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distance = cam.transform.position - transform.position;
+        if(distance.magnitude > distanceAway)
+        {
+            if (outline.enabled == true)
+            {
+                outline.enabled = false;
+            }
+        }
+        else
+        {
+            if(outline.enabled == false)
+            {
+                outline.enabled = true;
+            }
+        }
+    }
+}
