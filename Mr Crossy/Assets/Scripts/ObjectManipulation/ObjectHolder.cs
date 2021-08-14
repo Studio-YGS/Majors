235bf50a9dbfb7abb2a0572fc70b424d6bc166d6
Jack <1018019@student.sae.edu.au>
2021-08-14 21:52:55 +1000

    created dissolve and spawn back features

--- Mr Crossy/Assets/Scripts/ObjectManipulation/ObjectHolder.cs
+++ Mr Crossy/Assets/Scripts/ObjectManipulation/ObjectHolder.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class ObjectHolder : MonoBehaviour
 {
+    Vector3 startPos;
+    Quaternion startRot;
+    Material mat;
+    float dissolveValue;
     Transform hand;
     Transform objectInspectPoint;
     Transform cam;
     static bool objectHeld = false;
+    bool dissolving;
     [HideInInspector] public bool thisObjectHeld;
     [HideInInspector] public bool isPlacedDown;
     Vector3 posLastFrame;
@@ -21,6 +26,9 @@ public class ObjectHolder : MonoBehaviour
 
     void Start()
     {
+        mat = gameObject.GetComponent<MeshRenderer>().material;
+        startPos = transform.position;
+        startRot = transform.rotation;
         hand = GameObject.FindGameObjectWithTag("Hand").transform;
         objectInspectPoint = hand.GetComponentInChildren<Transform>();
         cam = FindObjectOfType<Camera>().transform;
@@ -37,6 +45,9 @@ public class ObjectHolder : MonoBehaviour
             {
                 if (Input.GetKeyDown(KeyCode.E) && !objectHeld)
                 {
+                    StopCoroutine("Dissolve");
+                    dissolveValue = 0;
+                    mat.SetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e", dissolveValue);
                     transform.parent = hand;
                     transform.position = hand.position;
                     gameObject.GetComponent<Collider>().enabled = false;
@@ -74,6 +85,7 @@ public class ObjectHolder : MonoBehaviour
                 gameObject.layer = 0;
                 objectHeld = false;
                 thisObjectHeld = false;
+                StartCoroutine("Dissolve");
                 if(controller.enabled == false)
                 {
                     Cursor.visible = true;
@@ -116,12 +128,43 @@ public class ObjectHolder : MonoBehaviour
 
         }
 
-        
+        if(dissolving == true)
+        {
+            dissolveValue += Time.deltaTime;
+            mat.SetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e", dissolveValue);
+        }
+        else if(dissolving == false)
+        {
+            if (mat.GetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e") != 0)
+            {
+                dissolveValue -= Time.deltaTime;
+                mat.SetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e", dissolveValue);
+            }
+        }
 
     }
 
     public void PutObjectDown()
     {
         objectHeld = false;
+    }
+
+    IEnumerator Dissolve()
+    {
+        yield return new WaitForSeconds(5);
+        dissolving = true;
+        while(mat.GetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e") < 1)
+        {
+            yield return null;
+        }
+        transform.position = startPos;
+        transform.rotation = startRot;
+        dissolving = false;
+        while (mat.GetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e") > 0)
+        {
+            yield return null;
+        }
+        dissolveValue = 0;
+        mat.SetFloat("Vector1_1bfaaeffe0534a91a219fc6f2e1eae9e", dissolveValue);
     }
 }
