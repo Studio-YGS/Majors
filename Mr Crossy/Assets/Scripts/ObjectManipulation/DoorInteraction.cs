0c4fea4e8d1c94cd444eaf0b4f31baadbe7a2d4c
Jack <1018019@student.sae.edu.au>
2021-09-30 15:34:17 +1000

    push for working door script

--- Mr Crossy/Assets/Scripts/ObjectManipulation/DoorInteraction.cs
+++ Mr Crossy/Assets/Scripts/ObjectManipulation/DoorInteraction.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class DoorInteraction : MonoBehaviour
 {
+    public bool xForward;
+    public bool zForward;
     bool moveable = false;
     bool greaterThan;
     bool lessThan;
     bool equalTo = true;
+    bool touchingPlayerRight;
+    bool touchingPlayerLeft;
     Transform cam;
     public float rotationVal;
     Transform player;
     float angleRelativeToPlayer;
+    float relativeAngle;
     public float openSpeed = 10;
     void Start()
     {
@@ -23,6 +29,19 @@ public class DoorInteraction : MonoBehaviour
     void Update()
     {
         //Debug.Log(transform.localRotation.y);
+        //if (Input.GetKeyDown(KeyCode.H))
+        //{
+        //    if (angleRelativeToPlayer > 180 * 0.5f)
+        //    {
+        //        //BEHIND
+        //        Debug.Log("behind");
+        //    }
+        //    else if (angleRelativeToPlayer < 180 * 0.5f)
+        //    {
+        //        //INFRONT
+        //        Debug.Log("infront");
+        //    }
+        //}
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
         {
@@ -33,19 +52,27 @@ public class DoorInteraction : MonoBehaviour
                 {
                     moveable = true;
                     Vector3 direction = transform.parent.position - player.position;
-                    angleRelativeToPlayer = Vector3.Angle(direction, transform.parent.forward);
+                    if (xForward)
+                    {
+                        angleRelativeToPlayer = Vector3.Angle(direction, transform.parent.right);
+                    }
+                    else if (zForward)
+                    {
+                        angleRelativeToPlayer = Vector3.Angle(direction, transform.parent.forward);
+                    }
+                    
                 }
             }
         }
         if (Input.GetMouseButtonUp(0) && moveable)
         {
-            if (transform.localRotation.y > 0.04)
+            if (transform.localRotation.y > 0.02)
             {
                 greaterThan = true;
                 lessThan = false;
                 equalTo = false;
             }
-            else if (transform.localRotation.y < -0.04)
+            else if (transform.localRotation.y < -0.02)
             {
                 greaterThan = false;
                 lessThan = true;
@@ -70,39 +97,54 @@ public class DoorInteraction : MonoBehaviour
                 //BEHIND
                 if (greaterThan)
                 {
-                    if (Input.GetAxis("Mouse X") > 0)
+                    if (Input.GetAxis("Mouse X") > 0 && !touchingPlayerLeft)
                     {
                         rotationVal += Input.GetAxis("Mouse X") * openSpeed;
 
                     }
-                    else if (Input.GetAxis("Mouse X") < 0)
+                    else if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerRight)
                     {
                         rotationVal -= -Input.GetAxis("Mouse X") * openSpeed;
                     }
                 }
                 else if (lessThan)
                 {
-                    if (Input.GetAxis("Mouse X") < 0)
+                    if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerLeft)
                     {
                         rotationVal += -Input.GetAxis("Mouse X") * openSpeed;
 
                     }
-                    else if (Input.GetAxis("Mouse X") > 0)
+                    else if (Input.GetAxis("Mouse X") > 0   && !touchingPlayerRight)
                     {
                         rotationVal -= Input.GetAxis("Mouse X") * openSpeed;
                     }
                 }
                 else if (equalTo)
                 {
-                    if (Input.GetAxis("Mouse X") < 0)
+                    if (zForward)
                     {
-                        rotationVal += -Input.GetAxis("Mouse X") * openSpeed;
+                        if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerRight)
+                        {
+                            rotationVal += -Input.GetAxis("Mouse X") * openSpeed;
 
+                        }
+                        else if (Input.GetAxis("Mouse X") > 0 && !touchingPlayerLeft)
+                        {
+                            rotationVal -= Input.GetAxis("Mouse X") * openSpeed;
+                        }
                     }
-                    else if (Input.GetAxis("Mouse X") > 0)
+                    else if (xForward)
                     {
-                        rotationVal -= Input.GetAxis("Mouse X") * openSpeed;
-                    }
+                        if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerLeft)
+                        {
+                            rotationVal += -Input.GetAxis("Mouse X") * openSpeed;
+
+                        }
+                        else if (Input.GetAxis("Mouse X") > 0  && !touchingPlayerRight)
+                        {
+                            rotationVal -= Input.GetAxis("Mouse X") * openSpeed;
+                        }
+                    } 
                 }
             }
             else if (angleRelativeToPlayer < 180 * 0.5f)
@@ -110,53 +152,97 @@ public class DoorInteraction : MonoBehaviour
                 //INFRONT
                 if (greaterThan)
                 {
-                    if (Input.GetAxis("Mouse X") < 0)
+                    if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerLeft)
                     {
                         rotationVal += -Input.GetAxis("Mouse X") * openSpeed;
 
                     }
-                    else if (Input.GetAxis("Mouse X") > 0)
+                    else if (Input.GetAxis("Mouse X") > 0  && !touchingPlayerRight)
                     {
                         rotationVal -= Input.GetAxis("Mouse X") * openSpeed;
                     }
                 }
                 else if (lessThan)
                 {
-                    if (Input.GetAxis("Mouse X") > 0)
+                    if (Input.GetAxis("Mouse X") > 0 && !touchingPlayerLeft)
                     {
                         rotationVal += Input.GetAxis("Mouse X") * openSpeed;
 
                     }
-                    else if (Input.GetAxis("Mouse X") < 0)
+                    else if (Input.GetAxis("Mouse X") < 0  && !touchingPlayerRight)
                     {
                         rotationVal -= -Input.GetAxis("Mouse X") * openSpeed;
                     }
                 }
                 else if (equalTo)
                 {
-                    if (Input.GetAxis("Mouse X") > 0)
+                    if (zForward)
                     {
-                        rotationVal += Input.GetAxis("Mouse X") * openSpeed;
+                        if (Input.GetAxis("Mouse X") > 0 && !touchingPlayerLeft)
+                        {
+                            rotationVal += Input.GetAxis("Mouse X") * openSpeed;
 
+                        }
+                        else if (Input.GetAxis("Mouse X") < 0 && !touchingPlayerRight)
+                        {
+                            rotationVal -= -Input.GetAxis("Mouse X") * openSpeed;
+                        }
                     }
-                    else if (Input.GetAxis("Mouse X") < 0)
+                    else if (xForward)
                     {
-                        rotationVal -= -Input.GetAxis("Mouse X") * openSpeed;
+                        if (Input.GetAxis("Mouse X") > 0 && !touchingPlayerRight)
+                        {
+                            rotationVal += Input.GetAxis("Mouse X") * openSpeed;
+
+                        }
+                        else if (Input.GetAxis("Mouse X") < 0  && !touchingPlayerLeft)
+                        {
+                            rotationVal -= -Input.GetAxis("Mouse X") * openSpeed;
+                        }
                     }
+                    
                 }
-            }
-            if (Input.GetAxis("Mouse X") > 0)
-            {
-                
-
-            }
-            else if (Input.GetAxis("Mouse X") < 0)
-            {
-                
             }
             transform.localRotation = Quaternion.Euler(0,  rotationVal , 0);
 
 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player.gameObject)
+        {
+            Debug.Log("oof");
+            Vector3 direction = transform.position - player.position;
+            if (xForward)
+            {
+                relativeAngle = Vector3.Angle(direction, transform.right);
+            }
+            else if (zForward)
+            {
+                relativeAngle = Vector3.Angle(direction, transform.forward);
+            }
+
+            if (relativeAngle > 180 * 0.5f)
+            {
+                touchingPlayerRight = true;
+            }
+            else if (relativeAngle < 180 * 0.5f)
+            {
+                touchingPlayerLeft = true;
+            }
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == player.gameObject)
+        {
+            Debug.Log("oof2");
+            touchingPlayerRight = false;
+            touchingPlayerLeft = false;
         }
     }
 }
