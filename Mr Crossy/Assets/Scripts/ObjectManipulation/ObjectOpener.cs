7fc54ff27a5e8858e77b68abf53417a122278c4e
Jack <1018019@student.sae.edu.au>
2021-10-06 20:24:40 +1100

    temp inclusion of rotational change when using secondary interaction

--- Mr Crossy/Assets/Scripts/ObjectManipulation/ObjectOpener.cs
+++ Mr Crossy/Assets/Scripts/ObjectManipulation/ObjectOpener.cs
@@ -8,6 +8,7 @@ public class ObjectOpener : MonoBehaviour
     ObjectHolder holder;
     GameObject pressE;
     Transform player;
+    Vector3 direction;
     float angleXRelativeToPlayer;
     float angleYRelativeToPlayer;
     bool turnOff;
@@ -16,6 +17,7 @@ public class ObjectOpener : MonoBehaviour
     public UnityEvent FirstObjectInteractionActions;
     public float xAngle;
     public float yAngle;
+    public Vector3 rotationOffset;
     void Start()
     {
         holder = GetComponent<ObjectHolder>();
@@ -26,15 +28,16 @@ public class ObjectOpener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, -transform.right, Color.green);
-        Debug.DrawRay(transform.position, player.position - transform.position, Color.red);
+        //Debug.DrawRay(transform.position, -transform.right, Color.green);
+        //Debug.DrawRay(transform.position, player.position - transform.position, Color.red);
         //Debug.Log(angleXRelativeToPlayer);
         //Debug.Log(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x);
         //Debug.Log(Quaternion.Euler(0, 0, 30).z);
         //Debug.Log(transform.localRotation.z);
+
         if (holder.beingInspected)
         {
-            Vector3 direction = player.position - transform.position;
+            direction = player.position - transform.position;
             angleXRelativeToPlayer = Vector3.Angle(direction, -transform.right);
             angleYRelativeToPlayer = Vector3.Angle(direction, transform.up);
             if (angleXRelativeToPlayer < xAngle * 0.5f  /*&& angleYRelativeToPlayer < yAngle * 0.5f*/ &&
@@ -51,6 +54,9 @@ public class ObjectOpener : MonoBehaviour
                     FirstObjectInteractionActions.Invoke();
                     pressed = true;
                     pressE.SetActive(false);
+                    holder.enabled = false;
+                    StartCoroutine(RotateToNewPosition());
+                    
                 }
             }
             else if (turnOff)
@@ -58,11 +64,24 @@ public class ObjectOpener : MonoBehaviour
                 pressE.SetActive(false);
                 turnOff = false;
             }
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized + rotationOffset), 0.01f);
         }
         else if (turnOff)
         {
             pressE.SetActive(false);
             turnOff = false;
+        }
+
+        IEnumerator RotateToNewPosition()
+        {
+            while (transform.rotation != Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized /*+ rotationOffset*/), 0.01f))
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized)/* + rotationOffset*/, 0.01f);
+                //Vector3 posOffset = transform.position - transform.GetComponent<Renderer>().bounds.center;
+                //transform.position = Vector3.Lerp(transform.position, player.position + player.forward * holder.distanceFromFace + posOffset, 0.01f);
+                yield return null;
+            }
+            holder.enabled = true;
         }
     }
 }
