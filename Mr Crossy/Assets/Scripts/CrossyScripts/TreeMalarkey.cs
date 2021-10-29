594c6988dd25f710cd2a28552d20339b7e2dfbd0
HosekiAnankos <jaspervonriegen@gmail.com>
2021-10-29 15:52:31 +1100

    Trying to make crossy event methods static

---
+++ Mr Crossy/Assets/Scripts/CrossyScripts/TreeMalarkey.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public static class TreeMalarkey
+{
+    public static void EnableTree(BehaviorTree tree)
+    {
+        tree.EnableBehavior();
+    }
+
+    public static void DisableTree(BehaviorTree tree)
+    {
+        tree.DisableBehavior();
+    }
+
+    public static void DisableTree(BehaviorTree tree, bool pauseTree)
+    {
+        tree.DisableBehavior(pauseTree);
+    }
+
+    public static void SendEventToTree(BehaviorTree tree, string eventName)
+    {
+        tree.SendEvent(eventName);
+    }
+    public static void SendEventToTree(BehaviorTree tree, string eventName, Object arg1)
+    {
+        tree.SendEvent(eventName, arg1);
+    }
+    public static void SendEventToTree(BehaviorTree tree, string eventName, Object arg1, Object arg2)
+    {
+        tree.SendEvent(eventName, arg1, arg2);
+    }
+    public static void SendEventToTree(BehaviorTree tree, string eventName, Object arg1, Object arg2, Object arg3)
+    {
+        tree.SendEvent(eventName, arg1, arg2, arg3);
+    }
+
+    public static void RegisterEventOnTree(BehaviorTree tree, string eventName, System.Action action)
+    {
+        tree.RegisterEvent(eventName, action);
+    }
+
+    public static void UnregisterEventOnTree(BehaviorTree tree, string eventName, System.Action action)
+    {
+        tree.UnregisterEvent(eventName, action);
+    }
+}
