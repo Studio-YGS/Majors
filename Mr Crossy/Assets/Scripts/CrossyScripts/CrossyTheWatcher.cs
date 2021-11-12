e98b895fc45380f4ae4fed04ada9fe51fe2b5976
HosekiAnankos <jaspervonriegen@gmail.com>
2021-11-13 01:01:39 +1100

    CrossyTitan Lines

--- Mr Crossy/Assets/Scripts/CrossyScripts/CrossyTheWatcher.cs
+++ Mr Crossy/Assets/Scripts/CrossyScripts/CrossyTheWatcher.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CrossyTheWatcher : MonoBehaviour
 {
-    Animator animator;
+    [HideInInspector] public Animator animator;
 
     public bool conditionIsTrue;
     public bool lerpPosition;
