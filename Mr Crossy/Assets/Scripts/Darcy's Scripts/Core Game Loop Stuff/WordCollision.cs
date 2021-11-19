d495953951ac2a91db0ebf5f6a0194df0c0b032e
Darcy Glover <67358033+darcyglover@users.noreply.github.com>
2021-11-19 18:09:20 +1100

    overlapping altars modular

--- Mr Crossy/Assets/Scripts/Darcy's Scripts/Core Game Loop Stuff/WordCollision.cs
+++ Mr Crossy/Assets/Scripts/Darcy's Scripts/Core Game Loop Stuff/WordCollision.cs
@@ -9,8 +9,8 @@ public class WordCollision : MonoBehaviour
     [HideInInspector]
     public bool puzzleComplete;
 
-    [SerializeField, Tooltip("The object on the canvas with the TMP components as its children")]
-    GameObject wordObject;
+    [SerializeField]
+    GameObject[] wordObjects;
 
     [SerializeField]
     PuzzleController puzzleController;
@@ -32,24 +32,43 @@ public class WordCollision : MonoBehaviour
     {
         if (other.CompareTag("GameController"))
         {
-            puzzleController.wordObject = wordObject;
-            puzzleController.word = word;
-            puzzleController.currentStreet = street;
-            puzzleController.SetUpLetters();
-            puzzleController.storedObjects.Clear();
-            puzzleController.wordCollision = gameObject.GetComponent<WordCollision>();
-            if (!puzzleComplete)
-            {
-                puzzleController.PlayerWordControl();
-            }
-
-            menuManager.streetName.SetActive(true);
-
-            FindObjectOfType<OverseerController>().m_StalkStreet = streetStalk;
-
-            respawn.RespawnColliders();
-
-            gameObject.SetActive(false);
+            SetUpController();
         }
+    }
+
+    void SetUpController()
+    {
+        int wordPoint = 0;
+
+        puzzleController.word = word;
+        puzzleController.wordObjects.Clear();
+
+        for (int i = 0; i < wordObjects.Length; i++)
+        {
+            puzzleController.wordObjects.Add(wordObjects[i]);
+
+            if(wordObjects[i].name == word)
+            {
+                wordPoint = i;
+            }
+        }
+
+        puzzleController.currentStreet = street;
+        puzzleController.SetUpLetters(wordPoint);
+        puzzleController.storedObjects.Clear();
+        puzzleController.wordCollision = gameObject.GetComponent<WordCollision>();
+
+        if (!puzzleComplete)
+        {
+            puzzleController.PlayerWordControl();
+        }
+
+        menuManager.streetName.SetActive(true);
+
+        FindObjectOfType<OverseerController>().m_StalkStreet = streetStalk;
+
+        respawn.RespawnColliders();
+
+        gameObject.SetActive(false);
     }
 }
