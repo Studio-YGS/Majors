f1b5661555a7151f2453e3342401444fc0d36f52
Jack <1018019@student.sae.edu.au>
2021-12-06 18:59:07 +1100

    more stuff done

--- Mr Crossy/Assets/Scripts/Audio_Scripts/NostalgiaBox.cs
+++ Mr Crossy/Assets/Scripts/Audio_Scripts/NostalgiaBox.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class NostalgiaBox : MonoBehaviour
 {
-
+    public StudioEventEmitter nostalgia;
+    public StudioEventEmitter homeDrone;
+    bool triggered;
     private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "GameController")
+        {
+            if (!triggered)
+            {
+                nostalgia.Play();
+                homeDrone.Stop();
+                triggered = true;
+            }
+
+        }
         
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "GameController")
+        {
+            if (other.transform.position.y < transform.position.y)
+            {
+                nostalgia.Stop();
+                homeDrone.Play();
+                triggered = false;
+            }
+        }
     }
 }
